514b3bd3bdfa17293c4c114d23b0c8efce7109ef
rjjackson22 <38430762+rjjackson22@users.noreply.github.com>
2026-05-22 15:27:27 +0000

    Automatic frontend build

--- public/build/4d3b2f5c-87a8-47b2-b53a-d046fb51d3d6/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/4d3b2f5c-87a8-47b2-b53a-d046fb51d3d6/static/js/remoteEntry.js'
-    
---
+++ public/build/ed3e3dad-49d5-4a7d-be0d-394c0e28ad3a/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/ed3e3dad-49d5-4a7d-be0d-394c0e28ad3a/static/js/remoteEntry.js'
+    
